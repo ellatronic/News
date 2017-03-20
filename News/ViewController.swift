88bbb44484b0f83e7c867f11dcfc1d8cb692569f
ellatronic <ella@ellatronic.com>
2017-03-20 09:23:46 -0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ella on 3/20/17.
+//  Copyright © 2017 Ellatronic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
